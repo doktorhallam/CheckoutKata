ff6ccb251065776d6343448c15b9aef483f2c999
Christopher Hallam <doktorhallam@gmail.com>
2016-10-18 22:33:53 -0400

    Autofac DI add, logic

---
+++ CheckoutKata.Web.Service/AppStart/DependencyConfig.cs
@@ -0,0 +1,39 @@
+﻿using Autofac;
+using Autofac.Integration.Mvc;
+using Autofac.Integration.WebApi;
+using CheckoutKata.Business.Providers;
+using System.Reflection;
+using System.Web.Http;
+using System.Web.Mvc;
+
+namespace CheckoutKata.Web.Service
+{
+    public static class DependencyConfig
+    {
+        public static void RegisterDependencies()
+        {
+            var builder = new ContainerBuilder();
+
+            // Provider registration
+            builder
+                .Register(
+                b => new ProductProvider())
+                .AsSelf();
+
+            builder
+                .Register(
+                b => new CartProvider())
+                .SingleInstance()
+                .AsSelf();
+
+            // Controller registration
+            builder
+                .RegisterApiControllers(Assembly.GetExecutingAssembly());
+
+            var container = builder.Build();
+
+            DependencyResolver.SetResolver(new AutofacDependencyResolver(container)); // Mvc Resolver
+            GlobalConfiguration.Configuration.DependencyResolver = new AutofacWebApiDependencyResolver(container); // WebApi Resolver
+        }
+    }
+}
---
+++ CheckoutKata.Web.Service/Global.asax.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Web;
+using System.Web.Http;
+
+namespace CheckoutKata.Web.Service
+{
+    public class Global : HttpApplication
+    {
+
+        protected void Application_Start(object sender, EventArgs e)
+        {
+            GlobalConfiguration.Configure(WebApiConfig.Register);
+            DependencyConfig.RegisterDependencies();
+        }
+        
+    }
+}
